76c41b9ee9086d597f40db386c9901c9db57e91e
Timothy Kelly <kelltimo@oregonstate.edu>
2017-11-17 20:52:30 -0700

    Ports

--- index.js
+++ index.js
@@ -78,6 +78,6 @@ app.get("/blog/:id/edit", function(req, res){
 
 
 
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+app.listen(process.env.PORT || 8000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
